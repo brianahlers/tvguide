704162565ae5e950bc96f92a6a5bddf809471a89
Brian A <brian.ahlers@gmail.com>
2024-02-21 16:02:09 -0800

    building page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
+import style from "./style.module.css";
 export function App() {
-    return <h1>Hello, world!</h1>;
+  return (
+    <div className={style.main_container}>
+      <div className={style.header}>
+        <div className="row">
+          <div className="col-4">
+            <div>LOGO</div>
+            <div>SUBTITLE</div>
+          </div>
+          <div className="col-md-12 col-lg-4">
+            <input style={{ width: "50%" }} type="text" />
+          </div>
+        </div>
+      </div>
+      <div className={style.tv_show_detail}>SHOW DETAIL</div>
+      <div className={style.recommend_tv_shows}>RECOMMENDED SHOWS</div>
+    </div>
+  );
 }
