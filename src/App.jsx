f48de2a288a9a78ef199d426f9d0f4dbe34eb058
Brian A <brian.ahlers@gmail.com>
2024-02-23 17:25:06 -0800

    set up homepage. display background image from searched item

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
+import { useEffect, useState } from "react";
 import { TVShowAPI } from "./api/tv-show";
-
 import style from "./style.module.css";
-
-TVShowAPI.fetchPopular();
+import { BACKDROP_BASE_URL } from "./api/config";
 
 export function App() {
+  const [currentShow, setCurrentShow] = useState();
+
+async function fetchPopular() {
+  const popularShows = await TVShowAPI.fetchPopular();
+  if (popularShows.length > 0 ) {
+    setCurrentShow(popularShows[0]);
+  }
+}
+
+  useEffect(() => {
+    fetchPopular();
+  }, []);
+
+
+  console.log(currentShow);
   return (
-    <div className={style.main_container}>
+    <div className={style.main_container}
+    style={{ background: 
+    currentShow ?
+    `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url("${ BACKDROP_BASE_URL }${ currentShow.backdrop_path }")
+    no-repeat center / cover`
+  : 'black'
+   }}>
       <div className={style.header}>
         <div className="row">
           <div className="col-4">
---
+++ src/api/config.js
@@ -0,0 +1,5 @@
+const BASE_URL = "https://api.themoviedb.org/3/discover/tv"
+const API_KEY_PARAM = "?api_key=42e443691af08774efb026edb81913b8"
+const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original"
+
+export { BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL }
